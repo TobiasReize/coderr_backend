7de79e88e13f3ceeace33c6fe55479f392c9ce4c
Tobias Reize <reizeto@web.de>
2025-01-16 11:25:26 +0100

    splitted OfferDetailSerializer to OfferRetrieveDeleteSerializer and OfferUpdateSerializer, updated update function and OfferViews, added default SessionAuthentication

--- coderr_app/api/serializers.py
+++ coderr_app/api/serializers.py
@@ -35,12 +35,12 @@ class OfferGetAdditionalFieldsSerializer(serializers.ModelSerializer):
         fields = ['min_price', 'min_delivery_time', 'user_details']
 
     def get_user_details(self, obj):
-        user = obj.user
-        return {
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'username': user.username
-        }
+        if obj.user:
+            return {
+                'first_name': obj.user.first_name,
+                'last_name': obj.user.last_name,
+                'username': obj.user.username
+            }
 
 
 class OfferListSerializer(OfferGetAdditionalFieldsSerializer, serializers.ModelSerializer):
@@ -49,34 +49,6 @@ class OfferListSerializer(OfferGetAdditionalFieldsSerializer, serializers.ModelS
     class Meta:
         model = Offer
         fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details', 'min_price', 'min_delivery_time', 'user_details']
-
-
-class OfferDetailSerializer(OfferGetAdditionalFieldsSerializer, serializers.ModelSerializer):
-    details = DetailedOfferSerializer(many=True)
-
-    class Meta:
-        model = Offer
-        fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details', 'min_price', 'min_delivery_time', 'user_details']
-        read_only_fields = ['user', 'created_at', 'updated_at']
-    
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-
-        if 'details' in validated_data:
-            offer_details_data = validated_data.pop('details')
-            for detail_data in offer_details_data:
-                type = detail_data.get('offer_type')
-                detail_instance = instance.details.get(offer_type=type)
-                detail_instance.title = detail_data.get('title', detail_instance.title)
-                detail_instance.revisions = detail_data.get('revisions', detail_instance.revisions)
-                detail_instance.delivery_time_in_days = detail_data.get('delivery_time_in_days', detail_instance.delivery_time_in_days)
-                detail_instance.price = detail_data.get('price', detail_instance.price)
-                detail_instance.features = detail_data.get('features', detail_instance.features)
-                detail_instance.save()
-
-        instance.save()
-        return instance
 
 
 class OfferCreateSerializer(serializers.ModelSerializer):
@@ -114,3 +86,62 @@ class OfferCreateSerializer(serializers.ModelSerializer):
         for field in fields_to_remove:
             representation.pop(field, None)
         return representation
+
+
+class OfferRetrieveDeleteSerializer(OfferGetAdditionalFieldsSerializer, serializers.ModelSerializer):
+    details = DetailedOfferSerializer(many=True)
+
+    class Meta:
+        model = Offer
+        fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details', 'min_price', 'min_delivery_time', 'user_details']
+        read_only_fields = ['user', 'created_at', 'updated_at']
+
+
+class OfferUpdateSerializer(serializers.ModelSerializer):
+    details = DetailedOfferSerializer(many=True)
+
+    class Meta:
+        model = Offer
+        fields = ['id', 'title', 'description', 'details']
+    
+    def update(self, instance, validated_data):
+        data = {}
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        data['id'] = instance.id
+        data['title'] = instance.title
+        data['description'] = instance.description
+
+        if 'details' in validated_data:
+            offer_details_data = validated_data.pop('details')
+            for detail_data in offer_details_data:
+                type = detail_data.get('offer_type')
+                detail_instance = instance.details.get(offer_type=type)
+                detail_instance.title = detail_data.get('title', detail_instance.title)
+                detail_instance.revisions = detail_data.get('revisions', detail_instance.revisions)
+                detail_instance.delivery_time_in_days = detail_data.get('delivery_time_in_days', detail_instance.delivery_time_in_days)
+                detail_instance.price = detail_data.get('price', detail_instance.price)
+                detail_instance.features = detail_data.get('features', detail_instance.features)
+                detail_instance.save()
+                
+                data['details'] = [
+                    {
+                        'id': detail_instance.id,
+                        'title': detail_instance.title,
+                        'revisions': detail_instance.revisions,
+                        'delivery_time_in_days': detail_instance.delivery_time_in_days,
+                        'price': detail_instance.price,
+                        'features': detail_instance.features,
+                        'offer_type': detail_instance.offer_type
+                    }
+                ]
+
+        instance.save()
+        return data
+    
+    # def to_representation(self, instance):
+    #     request_method = self.context['request'].method
+    #     if request_method == 'PATCH':
+    #         return self.handle_patch_representation(instance)
+    #     else:
+    #         return super().to_representation(instance)
--- coderr_app/api/views.py
+++ coderr_app/api/views.py
@@ -3,7 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Min
 
 from coderr_app.models import Offer, OfferDetail
-from .serializers import OfferCreateSerializer, DetailedOfferSerializer, OfferListSerializer, OfferDetailSerializer
+from .serializers import OfferCreateSerializer, DetailedOfferSerializer, OfferListSerializer, OfferRetrieveDeleteSerializer, OfferUpdateSerializer
 from .permissions import IsOwnerOrAdmin
 from .filters import CustomOfferFilter
 from .pagination import OfferPageNumberPagination
@@ -20,24 +20,27 @@ class OfferListCreateView(generics.ListCreateAPIView):
     def get_queryset(self):
         return Offer.objects.annotate(min_price=Min('details__price'), min_delivery_time=Min('details__delivery_time_in_days'))
 
-    def list(self, request, *args, **kwargs):
-        self.serializer_class = OfferListSerializer
-        return super().list(request, *args, **kwargs)
-    
-    def create(self, request, *args, **kwargs):
-        self.serializer_class = OfferCreateSerializer
-        return super().create(request, *args, **kwargs)
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return OfferCreateSerializer
+        else:
+            return OfferListSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
 
 class OfferDetailView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = OfferDetailSerializer
     permission_classes = [IsOwnerOrAdmin]
 
     def get_queryset(self):
         return Offer.objects.annotate(min_price=Min('details__price'), min_delivery_time=Min('details__delivery_time_in_days'))
+    
+    def get_serializer_class(self):
+        if self.request.method in ['PATCH', 'PUT']:
+            return OfferUpdateSerializer
+        else:
+            return OfferRetrieveDeleteSerializer
 
 
 class DetailedOfferView(generics.RetrieveAPIView):
