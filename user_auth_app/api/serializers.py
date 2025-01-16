b7861b7e2f89ffeecfb95c08f0dbb6052566f9d8
Tobias Reize <reizeto@web.de>
2025-01-16 15:11:48 +0100

    added UserProfile model, RegistrationView and RegistrationSerializer

---
+++ user_auth_app/api/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from user_auth_app.models import UserProfile
+from django.contrib.auth.models import User
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    repeated_password = serializers.CharField(write_only=True)
+    type = serializers.ChoiceField(choices=['customer', 'business'], write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'repeated_password', 'type']
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+    
+    def save(self):
+        print('self.validated_data:', self.validated_data)
+        username = self.validated_data['username']
+        email = self.validated_data['email']
+        pw = self.validated_data['password']
+        repeated_pw = self.validated_data['repeated_password']
+        type = self.validated_data['type']
+
+        if pw != repeated_pw:
+            raise serializers.ValidationError({'error': 'Passwords don\'t match'})
+        
+        try:
+            existing_user = User.objects.get(email=email)
+        except:
+            existing_user = None
+        
+        if existing_user:
+            print('existing User with that email: ', existing_user)
+            raise serializers.ValidationError({'error': 'Email already exists!'})
+        else:
+            account = User(username=username, email=email)
+            account.set_password(pw)
+            account.save()
+            user_profil = UserProfile.objects.create(user=account, username=username, type=type, email=email)
+            return account
--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -3,7 +3,6 @@ from .views import RegistrationView, CustomLoginView
 
 
 urlpatterns = [
-    # path('profile/', UserProfileList.as_view(), name='userprofile-list'),     # eine View um alle Profile anzuzeigen?
     # path('profile/<int:pk>/'),
     # path('profiles/business/'),
     # path('profiles/customers/'),
