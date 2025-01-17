502e5960a85c167e39ee4d6f05adf141575ea5fa
Tobias Reize <reizeto@web.de>
2025-01-17 14:29:09 +0100

    changed permission class IsProvider to IsProviderOrAdmin

--- coderr_app/api/permissions.py
+++ coderr_app/api/permissions.py
@@ -1,10 +1,10 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsProvider(BasePermission):
+class IsProviderOrAdmin(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
         elif request.method == 'POST':
-            return request.user.is_authenticated and (request.user.userprofile.type == 'business')
+            return (request.user.is_authenticated and request.user.is_superuser) or (request.user.is_authenticated and (request.user.userprofile.type == 'business'))
         return False
