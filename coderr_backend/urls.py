653d19fedd3fca8155e7a9f917951853bc68ef01
Tobias Reize <reizeto@web.de>
2025-02-01 10:26:14 +0100

    added static files

--- coderr_backend/urls.py
+++ coderr_backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -25,6 +26,6 @@ urlpatterns = [
     path('api/', include('coderr_app.api.urls')),
     path('api/', include('user_auth_app.api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-]
+] + staticfiles_urlpatterns()
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
